fb61dc496ca9a9a9f0022907e484a101425b67ef
alfinos <saviour.alfino@gmail.com>
2017-07-19 21:10:03 +0100

    First time commit

---
+++ app/src/main/java/com/example/salfino/naviblind_110217/RouteEntry.java
@@ -0,0 +1,125 @@
+package com.example.salfino.naviblind_110217;
+
+/**
+ * Created by Saviour on 16/07/2017.
+ */
+
+public class RouteEntry {
+
+    private String levelNo;
+    private String floorPlanId;
+    private String routedescription;
+    private String startpointname;
+    private String startpointtext;
+    private String firstwaypointname;
+    private String firstwaypointtext;
+    private String secondwaypointname;
+    private String secondwaypointtext;
+    private String endpointname;
+    private String endpointtext;
+
+    public String getLevelNo() {
+        return levelNo;
+    }
+
+    public void setLevelNo(String levelNo) {
+        this.levelNo = levelNo;
+    }
+
+    public String getFloorPlanId() {
+        return floorPlanId;
+    }
+
+    public void setFloorPlanId(String floorPlanId) {
+        this.floorPlanId = floorPlanId;
+    }
+
+    public String getRoutedescription() {
+        return routedescription;
+    }
+
+    public void setRoutedescription(String routedescription) {
+        this.routedescription = routedescription;
+    }
+
+    public String getStartpointname() {
+        return startpointname;
+    }
+
+    public void setStartpointname(String startpointname) {
+        this.startpointname = startpointname;
+    }
+
+    public String getStartpointtext() {
+        return startpointtext;
+    }
+
+    public void setStartpointtext(String startpointtext) {
+        this.startpointtext = startpointtext;
+    }
+
+    public String getFirstwaypointname() {
+        return firstwaypointname;
+    }
+
+    public void setFirstwaypointname(String firstwaypointname) {
+        this.firstwaypointname = firstwaypointname;
+    }
+
+    public String getFirstwaypointtext() {
+        return firstwaypointtext;
+    }
+
+    public void setFirstwaypointtext(String firstwaypointtext) {
+        this.firstwaypointtext = firstwaypointtext;
+    }
+
+    public String getSecondwaypointname() {
+        return secondwaypointname;
+    }
+
+    public void setSecondwaypointname(String secondwaypointname) {
+        this.secondwaypointname = secondwaypointname;
+    }
+
+    public String getSecondwaypointtext() {
+        return secondwaypointtext;
+    }
+
+    public void setSecondwaypointtext(String secondwaypointtext) {
+        this.secondwaypointtext = secondwaypointtext;
+    }
+
+    public String getEndpointname() {
+        return endpointname;
+    }
+
+    public void setEndpointname(String endpointname) {
+        this.endpointname = endpointname;
+    }
+
+    public String getEndpointtext() {
+        return endpointtext;
+    }
+
+    public void setEndpointtext(String endpointtext) {
+        this.endpointtext = endpointtext;
+    }
+
+    @Override
+    public String toString() {
+        return "RouteEntry{" +
+                "levelNo='" + levelNo + '\'' +
+                ", floorPlanId='" + floorPlanId + '\'' +
+                ", routedescription='" + routedescription + '\'' +
+                ", startpointname='" + startpointname + '\'' +
+                ", startpointtext='" + startpointtext + '\'' +
+                ", firstwaypointname='" + firstwaypointname + '\'' +
+                ", firstwaypointtext='" + firstwaypointtext + '\'' +
+                ", secondwaypointname='" + secondwaypointname + '\'' +
+                ", secondwaypointtext='" + secondwaypointtext + '\'' +
+                ", endpointname='" + endpointname + '\'' +
+                ", endpointtext='" + endpointtext + '\'' +
+                '}';
+    }
+}
