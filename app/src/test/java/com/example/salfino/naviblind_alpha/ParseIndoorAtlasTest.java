6893dd26e3fb71234498b65ffd1fdce7a0cf9718
alfinos <saviour.alfino@gmail.com>
2017-08-07 21:16:14 +0100

    Last changes before trials

---
+++ app/src/test/java/com/example/salfino/naviblind_alpha/ParseIndoorAtlasTest.java
@@ -0,0 +1,156 @@
+package com.example.salfino.naviblind_alpha;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Saviour on 25/07/2017.
+ */
+public class ParseIndoorAtlasTest {
+
+    private TextEntry myTextEntry;
+    private RoomEntry myRoomEntry;
+    private WayPointEntry myWaypointEntry;
+    private RoomWayPointEntry myRoomWaypointEntry;
+    private EventEntry myEventsEntry;
+    private RouteEntry myRouteEntry;
+
+    private ArrayList<TextEntry> textdata = new ArrayList<>();
+    private ArrayList<RoomEntry> roomdata = new ArrayList<>();
+    private ArrayList<WayPointEntry> waypointdata = new ArrayList<>();
+    private ArrayList<RoomWayPointEntry> roomwaypointdata = new ArrayList<>();
+    private ArrayList<EventEntry> eventdata = new ArrayList<>();
+    private ArrayList<RouteEntry> routedata = new ArrayList<>();
+
+    private ParseIndoorAtlas mParseIndoorAtlas = new ParseIndoorAtlas();
+
+
+    @Test
+    public void getRoomData() throws Exception {
+        myRoomEntry = new RoomEntry();
+        myRoomEntry.setLevelnumber("LevelNumber");
+        myRoomEntry.setFloorplanid("FloorPlanID");
+        myRoomEntry.setFloordescription("Description");
+        myRoomEntry.setName("Name");
+        myRoomEntry.setRoomdescription("Room");
+        myRoomEntry.setLatitude("Latitude");
+        myRoomEntry.setLongitude("Longitude");
+        myRoomEntry.setText("Text");
+
+        for (int i = 0; i < 20;i++){
+            roomdata.add(i,myRoomEntry);
+        }
+
+        mParseIndoorAtlas.setRoomdata(roomdata);
+        assertEquals(mParseIndoorAtlas.getRoomData(),roomdata);
+    }
+
+    @Test
+    public void getRouteData() throws Exception {
+        myRouteEntry = new RouteEntry();
+        myRouteEntry.setLevelNo("A");
+        myRouteEntry.setFloorPlanId("B");
+        myRouteEntry.setRoutedescription("C");
+        myRouteEntry.setStartpointname("D");
+        myRouteEntry.setStartpointtext("E");
+        myRouteEntry.setFirstwaypointname("F");
+        myRouteEntry.setFirstwaypointtext("G");
+        myRouteEntry.setFirstconfirmation("H");
+        myRouteEntry.setSecondwaypointname("I");
+        myRouteEntry.setSecondwaypointtext("J");
+        myRouteEntry.setSecondconfirmation("K");
+        myRouteEntry.setEndpointname("L");
+        myRouteEntry.setEndpointtext("M");
+
+        for (int i = 0; i < 20;i++){
+            routedata.add(i,myRouteEntry);
+        }
+
+        mParseIndoorAtlas.setRoutedata(routedata);
+        assertEquals(mParseIndoorAtlas.getRouteData(),routedata);
+
+    }
+
+    @Test
+    public void getTextData() throws Exception {
+        myTextEntry = new TextEntry();
+        myTextEntry.setIntrotext("Intro");
+        myTextEntry.setLocationtext("Location");
+        myTextEntry.setMenutext("Menu");
+        myTextEntry.setRoomtext("Room");
+        myTextEntry.setWaypointtext("Waypoint");
+        myTextEntry.setConfirmationtext("Confirmation");
+        myTextEntry.setRepeattext("Repeat");
+        myTextEntry.setServicetext("Service");
+        myTextEntry.setEventsintrotext("Events");
+        myTextEntry.setNoeventstext("NoEvents");
+        myTextEntry.setRoutetext("Route");
+        myTextEntry.setNoroutetext("NoRoute");
+        myTextEntry.setResumetext("Resume");
+
+        for (int i = 0; i < 20;i++){
+            textdata.add(i,myTextEntry);
+        }
+
+        mParseIndoorAtlas.setTextdata(textdata);
+        assertEquals(mParseIndoorAtlas.getTextData(),textdata);
+    }
+
+    @Test
+    public void getWayPointData() throws Exception {
+
+        myWaypointEntry = new WayPointEntry();
+        myWaypointEntry.setLevelnumber("LevelNumber");
+        myWaypointEntry.setFloorplanid("FloorPlanID");
+        myWaypointEntry.setFloordescription("Description");
+        myWaypointEntry.setName("Name");
+        myWaypointEntry.setWaypointdescription("Waypoint");
+        myWaypointEntry.setLatitude("Latitude");
+        myWaypointEntry.setLongitude("Longitude");
+        myWaypointEntry.setText("Text");
+
+        for (int i = 0; i < 20;i++){
+            waypointdata.add(i,myWaypointEntry);
+        }
+
+        mParseIndoorAtlas.setWaypointdata(waypointdata);
+        assertEquals(mParseIndoorAtlas.getWayPointData(),waypointdata);
+
+    }
+
+    @Test
+    public void getRoomWayPointData() throws Exception {
+
+        myRoomWaypointEntry = new RoomWayPointEntry();
+        myRoomWaypointEntry.setLevelnumber("LevelNumber");
+        myRoomWaypointEntry.setFloorplanid("FloorPlanID");
+        myRoomWaypointEntry.setFloordescription("Description");
+        myRoomWaypointEntry.setName("Name");
+        myRoomWaypointEntry.setDescription("Description");
+        myRoomWaypointEntry.setLatitude("Latitude");
+        myRoomWaypointEntry.setLongitude("Longitude");
+        myRoomWaypointEntry.setText("Text");
+
+        for (int i = 0; i < 20;i++){
+            roomwaypointdata.add(i,myRoomWaypointEntry);
+        }
+
+        mParseIndoorAtlas.setRoomwaypointdata(roomwaypointdata);
+        assertEquals(mParseIndoorAtlas.getRoomWayPointData(),roomwaypointdata);
+
+    }
+
+    @Test
+    public void getConfigurationData() throws Exception {
+
+    }
+
+    @Test
+    public void getEventData() throws Exception {
+
+    }
+
+}
---
+++ app/src/test/java/com/example/salfino/naviblind_alpha/TextEntryTest.java
@@ -0,0 +1,111 @@
+package com.example.salfino.naviblind_alpha;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Saviour on 25/07/2017.
+ */
+public class TextEntryTest {
+    private TextEntry textEntry;
+    //private ParseIndoorAtlas mParseIndoorAtlas;
+
+    @Before
+    public void setUp() throws Exception {
+        textEntry = new TextEntry();
+        //mParseIndoorAtlas = mock(ParseIndoorAtlas.class);
+
+    }
+
+    @Test
+    public void getAndsetIntrotext_check() throws Exception {
+        textEntry.setIntrotext("Intro");
+        assertEquals(textEntry.getIntrotext(),"Intro");
+    }
+
+    @Test
+    public void getAndsetLocationtext() throws Exception {
+        textEntry.setLocationtext("Location");
+        assertEquals(textEntry.getLocationtext(),"Location");
+
+    }
+
+    @Test
+    public void getAndsetMenutext() throws Exception {
+        textEntry.setMenutext("Menu");
+        assertEquals(textEntry.getMenutext(),"Menu");
+    }
+
+    @Test
+    public void getAndsetRoomtext() throws Exception {
+        textEntry.setRoomtext("Room");
+        assertEquals(textEntry.getRoomtext(),"Room");
+
+    }
+
+    @Test
+    public void getAndsetWaypointtext() throws Exception {
+        textEntry.setWaypointtext("Waypoint");
+        assertEquals(textEntry.getWaypointtext(),"Waypoint");
+
+    }
+
+    @Test
+    public void getAndsetConfirmationtext() throws Exception {
+        textEntry.setConfirmationtext("Confirmation");
+        assertEquals(textEntry.getConfirmationtext(),"Confirmation");
+
+    }
+
+    @Test
+    public void getAndsetRepeattext() throws Exception {
+        textEntry.setRepeattext("Repeat");
+        assertEquals(textEntry.getRepeattext(),"Repeat");
+
+    }
+
+    @Test
+    public void getAndsetServicetext() throws Exception {
+        textEntry.setServicetext("Service");
+        assertEquals(textEntry.getServicetext(),"Service");
+
+    }
+
+    @Test
+    public void getAndsetEventsintrotext() throws Exception {
+        textEntry.setEventsintrotext("Events");
+        assertEquals(textEntry.getEventsintrotext(),"Events");
+
+    }
+
+    @Test
+    public void getAndsetNoeventstext() throws Exception {
+        textEntry.setNoeventstext("NoEvents");
+        assertEquals(textEntry.getNoeventstext(),"NoEvents");
+
+    }
+
+    @Test
+    public void getAndsetRoutetext() throws Exception {
+        textEntry.setRoutetext("Route");
+        assertEquals(textEntry.getRoutetext(),"Route");
+
+    }
+
+    @Test
+    public void getAndsetNoroutetext() throws Exception {
+        textEntry.setNoroutetext("NoRoute");
+        assertEquals(textEntry.getNoroutetext(),"NoRoute");
+
+    }
+
+    @Test
+    public void getAndsetResumetext() throws Exception {
+        textEntry.setResumetext("Resume");
+        assertEquals(textEntry.getResumetext(),"Resume");
+
+    }
+
+}
